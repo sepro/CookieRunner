64577ea06d86c6be69d1ffabb7fd217c03539289
sepro <sebastian.proost@gmail.com>
2015-07-01 18:02:11 +0200

    getting the calories per cookie works and is loaded using ajax.
    Still lots of layout improvements to do

--- cookierun/controllers/cookie.py
+++ cookierun/controllers/cookie.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 from cookierun.models.cookies import Cookie
-
+from math import floor
 
 cookies = Blueprint('cookies', __name__)
 
@@ -15,3 +15,11 @@ def cookies_list():
 def cookies_view(cookie_id):
     cookie = Cookie.query.get_or_404(cookie_id)
     return render_template("cookies.html", cookie=cookie)
+
+@cookies.route('/count/<calories>')
+def cookies_count(calories):
+    output = {}
+    all_cookies = Cookie.query.all()
+    for cookie in all_cookies:
+        output[cookie.name] = floor(float(calories)/float(cookie.calories))
+    return jsonify(output)
